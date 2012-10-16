7afe3ca9251ac96c9de88790ebf60dd36faafdd5
Adonai <adonai@localhost.localdomain>
2012-10-16 16:29:42 +0100

    Se añade el metodo bfs. Cambios en los ids de los nodos. Se incrementa en uno para que se corresponda con la realidad

--- lib/graph.rb
+++ lib/graph.rb
@@ -37,11 +37,11 @@ class Graph
 			node_adjacency = []
 			adjacency[i].each_index do |j|
 				if adjacency[i][j] == "1"
-					node_adjacency.push(j)
+					node_adjacency.push(j+1)
 				end
 			end
 
-			@nodes.push(Node.new(i, node_adjacency))	# Creamos un nodo nuevo
+			@nodes.push(Node.new(i+1, node_adjacency))	# Creamos un nodo nuevo
 		end
 
 		f_in.close
@@ -80,11 +80,11 @@ class Graph
 	end
 
 	def h(from, to)
-		return @heuristic[from][to]
+		return @heuristic[from-1][to-1]
 	end
 
 	def cost(from, to)
-		return @cost[from][to]
+		return @cost[from-1][to-1]
 	end
 
 	def get_node_by_id(node_id)
@@ -107,7 +107,7 @@ class Graph
 			return
 		end
 
-		initial_node.predecessors.push(initial_node.id) 	# añadimos el id de su padre a la lista, que en este caso será el mismo nodo
+		#initial_node.predecessors.push(initial_node.id) 	# añadimos el id de su padre a la lista, que en este caso será el mismo nodo
 		to_be_analized.push(initial_node)
 		generated+=1
 
@@ -137,7 +137,6 @@ class Graph
 
 		if current_node != nil	# Falta mejorar la salida. En principio este metodo no mostraria los resultados. IDEA: Devolver un hash con los resultados
 
-			current_node.predecessors.delete_at(0)
 			current_node.predecessors.push(current_node.id)
 			puts "Se encontro el camino entre #{initial_node.id} y #{final_node.id}"
 			puts current_node.predecessors.join("->")
@@ -149,7 +148,55 @@ class Graph
 		end
 	end
 
-	def breadth_first_search
+	def breadth_first_search(initial_node, final_node)
+
+		generated = 0
+		analized = 0
+		to_be_analized = []
+
+		if initial_node.id == final_node.id
+			puts "El nodo inicial es el nodo final. No se genero un camino"
+			return
+		end
+
+		to_be_analized.push(initial_node)
+		generated+=1
+
+		current_node = to_be_analized.shift
+
+		while current_node != nil && current_node.id != final_node.id
+
+			analized += 1
+
+			current_node.children.each do |child_id|
+
+				if !current_node.predecessors.include?(child_id)
+					
+					node = get_node_by_id(child_id)							# obtenemos una copia del nodo
+					
+					node.predecessors = current_node.predecessors.clone		 	# añadimos al nodo que va a ser generado la lista de predecesores actualizado
+					node.predecessors.push(current_node.id)
+
+					node.cumulative_cost = current_node.cumulative_cost + cost(current_node.id, node.id)	# le añadimos al cobjeto su costo acumulado
+
+					to_be_analized.push(node)							 	# añade los hijos que no estan en el camino
+					generated += 1
+				end
+			end
+			current_node = to_be_analized.shift		# sacamos el siguiente nodo de la cola fifo
+		end
+
+		if current_node != nil	# Falta mejorar la salida. En principio este metodo no mostraria los resultados. IDEA: Devolver un hash con los resultados
+
+			current_node.predecessors.push(current_node.id)
+			puts "Se encontro el camino entre #{initial_node.id} y #{final_node.id}"
+			puts current_node.predecessors.join("->")
+			puts "Nodos analizados: #{analized}"
+			puts "Nodos generados:  #{generated}"
+			puts "Costo acumulado:  #{current_node.cumulative_cost}"
+		else
+			puts "No se encontro camino desde #{initial_node.id} a #{final_node.id}."
+		end
 	end
 
 	def a_star
@@ -159,4 +206,5 @@ end
 grafo = Graph.new
 
 #controlar el numero de los nodos desde el main. O no...
-grafo.deep_first_search( grafo.get_node_by_id(0), grafo.get_node_by_id(13) )
+grafo.deep_first_search( grafo.get_node_by_id(1), grafo.get_node_by_id(14) )
+grafo.breadth_first_search( grafo.get_node_by_id(1), grafo.get_node_by_id(14) )
